58663156caee1bd3aebc6929f4e8ed5d4280d8a3
Nrking913 <nrking913@comcast.net>
2023-06-12 16:52:05 -0700

    Added in Favorites redirect and react/redux

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,34 @@
 // Dependencies
 import React, { useState } from 'react';
-import Options from './options.jsx';
-import Generated from './generated.jsx';
-// import logo from '../logo.png'
 
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// Component imports
+import Home from './Home2.jsx'
+import Favorites from './favorites.jsx';
+// npm react
 const App = () => {
 
   //main container for the entire page
   // we have a star image which has an anchor link to the base domain at the moment, but will be refactored to '/favorites'
   // our logo is just a text logo made as a png, and it will serve as the main nav component that takes you back to the homepage
+  //main container for the entire page
   return (
-    <div className="container">
-
-      <div className="navBar">
-      <a href="/"><img src="https://i.ibb.co/hF8mRSN/Favorites-Star.png" alt="favorites"  className="favorites"></img></a>
-        {/* <img src= { require('../logo.png')} /> */}
-        <a href="/"><img src="https://i.ibb.co/ZJVR822/logo.png" alt="logo" border="0" className="logo"/></a>
+    <>
+      <div className="container">
+        <div className="navBar">
+          <Link to="/favorites"><img src="https://i.ibb.co/hF8mRSN/Favorites-Star.png" alt="favorites"  className="favorites" /></Link>
+          <Link className="Home" to="/"><img src="https://i.ibb.co/ZJVR822/logo.png" alt="logo" border="0" className="logo"/></Link>
         </div>
-      <div className="underNav">
-        <div className="leftColumn">
-          {/* <button>Favorites</button> */}
-        </div>
-        <div className="contentContainer">
-          <Options />
-          <Generated />
-        </div>
-        <div className="rightColumn"></div>
+        <Routes>
+          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/" element={<Home /> }/>
+        </Routes>
       </div>
     </>
   );
-}
+};
+
 
 
 export default App;
