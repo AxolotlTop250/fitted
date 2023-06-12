56ec2acb09d2d2d310d3decd9c383e4aec37114b
18gwoo <garrettwoogs@yahoo.com>
2023-06-12 16:20:43 -0700

    Co-authored-by: ChristopherPark3 <ChristopherPark3@users.noreply.github.com> Co-authored-by: Chris Suzukida <csuzukida@users.noreply.github.com>

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,31 +1,30 @@
+// Dependencies
 import React, { useState } from 'react';
-import Options from './options.jsx';
-import Generated from './generated.jsx';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// Component imports
+import Home from './Home.jsx'
+import Favorites from './favorites.jsx';
 // import logo from '../logo.png'
 
 const App = () => {
 
   //main container for the entire page
   return (
-    <div className="container">
-
-      <div className="navBar">
-      <a href="/"><img src="https://i.ibb.co/hF8mRSN/Favorites-Star.png" alt="favorites"  className="favorites"></img></a>
-        {/* <img src= { require('../logo.png')} /> */}
-        <a href="/"><img src="https://i.ibb.co/ZJVR822/logo.png" alt="logo" border="0" className="logo"/></a>
+    <>
+      <div className="container">
+        <div className="navBar">
+          <Link className="Favorites" to="/favorites"><img src="https://i.ibb.co/hF8mRSN/Favorites-Star.png" alt="favorites"  className="favorites" /></Link>
+          <Link className="Home" to="/"><img src="https://i.ibb.co/ZJVR822/logo.png" alt="logo" border="0" className="logo"/></Link> 
         </div>
-      <div className="underNav">
-        <div className="leftColumn">
-          {/* <button>Favorites</button> */}
-        </div>
-        <div className="contentContainer">
-          <Options />
-          <Generated />
-        </div>
-        <div className="rightColumn"></div>
+        <Routes>
+          <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/" element={<Home /> }/>
+        </Routes>
       </div>
-    </div>
+    </>
   );
-};
+}
+
 
 export default App;
